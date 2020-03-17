554ddf276051205b7d13649548ec116fcc70ec90
amiao-h <57279076+amiao-h@users.noreply.github.com>
2020-03-16 23:46:58 -0500

    version 2
    version 2

--- server.R
+++ server.R
@@ -1,131 +1,167 @@
+## Data Visual Final Project Group 5
+## By: Gokturk Demir, Lei Hong, Jiewen Wu, Yiyan Wan
+
+
 shinyServer(function(input, output,graphtype) {
-
-    train_shiny = read.csv('loan_visulization',sep = '/')
-    library(dplyr)
-    set.seed(49)
-    head(train_shiny)
-    str(train_shiny)
-    idx = sample(1:nrow(train_shiny),as.integer(nrow(train_shiny)*0.1),replace = FALSE)
-    sub_dat = train_shiny[idx,]
-    sub_dat = sub_dat %>% select(-one_of('X'))
-
-
-    output$prediction <- renderPrint({
+  
+  library(RCurl)
+  x=getURL('https://raw.githubusercontent.com/amiao-h/Shinyloan/master/loan_visulization.csv')
+  train_shiny = read.csv(text=x,sep = ',')
+  head(train_shiny)
+  sub_dat = train_shiny
+  str(sub_dat)
+  sub_dat['X1'] = substr(sub_dat$X1,
+                         start = 1, 
+                         stop= nchar(as.character(sub_dat$X1))-1) 
+  sub_dat$X1 = as.numeric(sub_dat$X1)
+  sub_dat$X4 = gsub('$', '', sub_dat$X4, fixed = TRUE)
+  sub_dat$X4 = as.numeric(sub(",", "", sub_dat$X4))
+    test = reactive({
+  
+            df <- data.frame(loan_amount = (input$X4),annual_income = (input$X13),state_of_residency = (input$X20),home_ownership = (input$X12))
+          list(df=df) 
         
-       if (input$X4 <= 50){
-           return(NULL)
-       }
-       else if(input$X13 < 0) {
-           return(NULL)
-       }
-       else{
-        test.data = data.frame(input$X4, input$X13, input$X20, input$X12)
-        colnames(test.data) = c("X4", "X13", "X20",'X12')
-        ###########################################################
-        ## load training data
-        ###########################################################
         
-
-        # perform Linear model
-        lm.fit = lm(X1 ~ X4 + X13 + X20  +  X12, data = sub_dat)
-        
-        # make predictions
-        lm.pred<-predict(lm.fit, test.data)
-        
-        paste("Based on your info, the estimated interest rate ", round(lm.pred[1],2),'%',sep='')
-        
-   } })
-    
-    
-        ####
+    })
     df_grade_interest = train_shiny[,c("X1", "X8")]
-        output$Graphs = renderPlot(
-            if (input$Graphtype == "Loan Category") {
-                value_count = as.data.frame(table(train_shiny$X17))
-                value_count$Freq = value_count$Freq/dim(train_shiny)[1]
-                a <-value_count[order(value_count$Freq),]
-                frequencytable = tail(a,4)
-                ggplot(frequencytable, aes(x="", y=Freq, fill=Var1)) + geom_bar(stat="identity", width=1) +
-                coord_polar("y", start=0) + geom_text(aes(label = paste0(round(Freq*100), "%")), position = position_stack(vjust = 0.5)) +
-                labs(x = NULL, y = NULL, fill = NULL, title = "Loan Category") +scale_fill_brewer(palette="PuBu")+
-                theme_classic() + theme(axis.line = element_blank(), axis.text = element_blank(), axis.ticks = element_blank(),plot.title = element_text(hjust = 0.5, color = "#666666"))
-            } else if (input$Graphtype == "Interest rate for each loan grade"){
-                boxplot(
-                    X1 ~ X8,
-                    data = df_grade_interest,
-                    main = "Interest rate for each loan grade",
-                    xlab = "Loan Grade",
-                    ylab = "Interest rate",
-                    col = "grey",
-                    border = "blue"
-                )}
-            else if (input$Graphtype == "plots of interest rate and income"){
-                    par(mfrow=c(2,2), las=1, bty="n")
-                    #histogram of interest rate
-                    
-                    hist(train_shiny$X1,
-                         main='Histogram of interest rate',
-                         xlab='interest rate',
-                         color = "grey",
-                         border = "blue")
-                    
-                    ##histogram of log income
-                    
-                    hist(train_shiny$logX13,
-                         main='Histogram of annual income',
-                         xlab='annual income',
-                         color = "grey",
-                         border = "blue",
-                         col='grey')
-                    
-                    ## boxplot of year of work
-                    #not good
-                    boxplot(X1~X11_new,
-                            main='interest rate by working years',
-                            xlab='number of years employed ',
-                            ylab='interest rate',
-                            color = "grey",
-                            border = "blue",
-                            data=train_shiny)
-                    
-                    
-                    boxplot(logX13~X8,
-                            main='income by working years',
-                            xlab='loan grade',
-                            ylab='log income',
-                            color = "grey",
-                            border = "blue",
-                            data=train_shiny)
-                }   
-            else if (input$Graphtype == "Number of Loan Requests by State"){
-                library('choroplethrMaps')
-                choroplethr::state_choropleth(df = map, title = "Number of Loan Requests by State")           
-            }
-            else if (input$Graphtype == "Interest Rate by Number of Payments"){
-                ggplot(train_shiny, aes(x=X1, fill=X7)) + geom_density(alpha=.5)+
-                    labs(title = "Interest Rate by Number of Payments", x = "Interest Rate",y = "Density") +
-                    scale_fill_brewer(palette="PuBu")+theme_classic()+ guides(fill=guide_legend(title=NULL))        
-            }
-            else if (input$Graphtype == "Frequency by Number of credit lines"){
-                df_number_of_credit = as.data.frame(table(train_shiny$X27))
-                df_2 = df_number_of_credit[order(df_number_of_credit$Freq),]
-                ggplot(df_2, aes(x = Var1, y = Freq)) +
-                    geom_bar(stat = "identity", color = "blue", fill = "grey") +
-                    labs(title = "Frequency by Number of credit lines\n", x = "\nNumber of credit lines", y = "Frequency\n") +
-                    theme_classic() + geom_smooth(method = "normal")           
-            }else if(input$Graphtype == "Number of Loan Requests by Home Ownership * Working years"){
-                home_owndership_grade=xtabs(X1~X11_new+X12,train_shiny)
-                mosaicplot(home_owndership_grade,ylab='Home Ownership',xlab='Number of years employed ',
-                           color=TRUE,border='blue',main=NULL,las=1)
-            }
-            
-            )
-
+    
+    output$pred <- renderPrint({
         
-        })
+        if (input$X4 <= 50){
+            return(NULL)
+        }
+        else if(input$X13 < 0) {
+            return(NULL)
+        }
+        else{
+            test.data = data.frame(input$X4, input$X13, input$X20, input$X12)
+            colnames(test.data) = c("X4", "X13", "X20",'X12')
+            ###########################################################
+            ## load training data
+            ###########################################################
+            
+            
+            # perform Linear model
+            lm.fit = lm(X1 ~ X4 + X13 + X20  +  X12, data = sub_dat)
+            
+            # make predictions
+            lm.pred<-predict(lm.fit, test.data)
+            
+            paste("Based on your info, the estimated interest rate is ", round(lm.pred[1],2),'%',sep='')
+            
+        } })
+    output$table = renderTable({
+        test()$df
+    })
+    output$view0 = renderPlotly({
+        df = num_loan_by_state
+        
+        # give state boundaries a white border
+        l <- list(color = toRGB("white"), width = 2)
+        # specify some map projection/options
+        g <- list(
+            scope = 'usa',
+            projection = list(type = 'albers usa'),
+            showlakes = TRUE,
+            lakecolor = toRGB('white')
+        )
+        
+        
+        fig <- plot_geo(df, locationmode = 'USA-states')
+        fig <- fig %>% add_trace(
+            z = ~Count,locations = ~State,
+            color = ~Count, colors = 'Blues'
+        )
+        fig <- fig %>% colorbar(title = "Number of Loan Requested")
+        fig <- fig %>% layout(
+            title = 'Amount of Loan Requested in Each State',
+            geo = g
+        )
+          fig
+    })
+    
+    output$view1 = renderPlot({
+        value_count = as.data.frame(table(train_shiny$X17))
+        value_count$Freq = value_count$Freq/dim(train_shiny)[1]
+        a <-value_count[order(value_count$Freq),]
+        frequencytable = tail(a,4)
+        ggplot(frequencytable, aes(x="", y=Freq, fill=Var1)) + geom_bar(stat="identity", width=1) +
+            coord_polar("y", start=0) + geom_text(aes(label = paste0(round(Freq*100), "%")), position = position_stack(vjust = 0.5)) +
+            labs(x = NULL, y = NULL, fill = NULL, title = "Loan Category") + scale_fill_brewer(palette="PuBu")+
+            theme_classic() + theme(axis.line = element_blank(), axis.text = element_blank(), axis.ticks = element_blank(),plot.title = element_text(hjust = 0.5, color = "#666666"))
 
-
-
-
-
-
+                 })
+    
+     output$view2 = renderPlot({
+         boxplot(
+             X1 ~ X8,
+             data = df_grade_interest,
+             main = "Interest rate for each loan grade",
+             xlab = "Loan Grade",
+             ylab = "Interest rate",
+             col = "grey",
+             border = "blue")
+     })
+    output$view3 = renderPlotly({
+        df_number_of_credit = as.data.frame(table(train_shiny$X27))
+        df_2 = df_number_of_credit[order(df_number_of_credit$Freq),]
+        g1 = ggplot(df_2, aes(x = Var1, y = Freq)) +
+            geom_bar(stat = "identity", color = "blue", fill = "grey") +
+            labs(title = "Frequency by Number of credit lines\n", x = "\nNumber of credit lines", y = "Frequency\n") +
+            theme_classic() + geom_smooth(method = "normal") 
+        ggplotly(g1)
+    })
+    
+    output$view4 = renderPlot({
+        ggplot(train_shiny, aes(x=X1, fill=X7)) + geom_density(alpha=.5)+
+            labs(title = "Interest Rate by Number of Payments", x = "Interest Rate",y = "Density") +
+            scale_fill_brewer(palette="PuBu")+theme_classic()+ guides(fill=guide_legend(title=NULL))        
+        
+    })
+    output$view5 = renderPlot({
+        home_owndership_grade=xtabs(X1~X11_new+X12,train_shiny)
+        mosaicplot(home_owndership_grade,ylab='Home Ownership',xlab='Number of years employed ',
+                   color=TRUE,border='blue',main=NULL,las=1)
+    })
+    output$view6 = renderPlot({
+        par(mfrow=c(2,2), las=1, bty="n")
+        #histogram of interest rate
+        
+        hist(train_shiny$X1,
+             main='Histogram of interest rate',
+             xlab='interest rate',
+             color = "grey",
+             border = "blue")
+        
+        ##histogram of log income
+        
+        hist(train_shiny$logX13,
+             main='Histogram of annual income',
+             xlab='annual income',
+             color = "grey",
+             border = "blue",
+             col='grey')
+        
+        ## boxplot of year of work
+        #not good
+        boxplot(X1~X11_new,
+                main='interest rate by working years',
+                xlab='number of years employed ',
+                ylab='interest rate',
+                color = "grey",
+                border = "blue",
+                data=train_shiny)
+        
+        
+        boxplot(logX13~X8,
+                main='income by working years',
+                xlab='loan grade',
+                ylab='log income',
+                color = "grey",
+                border = "blue",
+                data=train_shiny)
+      
+    
+    })
+    })
+    
--- ui.R
+++ ui.R
@@ -1,39 +1,132 @@
+
+# Data Visualization Final Project
+# By: Gokturk Demir,  Lei Hong, Jiewen Wu, Yiyan Wan
+#
+
+library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(ggthemes)
+library(party)
+library(caret)
+library(plotly)
+library(curl)
+library(RCurl)
+library(devtools)
+install_github("nik01010/dashboardthemes")
+library(plotly)
+
+
+
 shinyUI(
-    ui=fluidPage(
-        headerPanel("Interest Rate Predictor"),
-        tabsetPanel(
-            tabPanel('Prediction',
-                     sidebarPanel(
-                         selectInput("X20", "Choose your state of residency:", 
-                                     choices = c( "AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD",
-                                                  "ME","MI","MN","MO","MS","MT","NC","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX",
-                                                  "UT","VA","VT","WA","WI","WV","WY")),
-                         numericInput("X13", "Annual Income of borrower:", 1000, min = 1),
-                         numericInput("X4", "Loan amount requested:", 1000, min = 100),
-                         selectInput('X12','Home Ownership Status:',choices = c("MORTGAGE","NONE","OTHER","OWN","RENT")),
-                         submitButton("Calculate")
-                     ),
-                     mainPanel(
+ui <- dashboardPage(
+                    dashboardHeader(title = "QuickLoan Services"
+                    ),
+                    dashboardSidebar(
+                        sidebarMenu(id = 'sidebarmenu',
+                            menuItem("Interest Rate Calculator", tabName = "Prediction", icon = icon("dashboard")),
+                            conditionalPanel("input.sidebarmenu =='Prediction'", selectInput("X20", "Choose your state of residency:", 
+                                                                                             choices = c( "AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD",
+                                                                                                          "ME","MI","MN","MO","MS","MT","NC","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX",
+                                                                                                          "UT","VA","VT","WA","WI","WV","WY")),
+                                             numericInput("X13", "Annual Income of borrower:", 1000, min = 1),
+                                             numericInput("X4", "Loan amount requested:", 1000, min = 100),
+                                             selectInput('X12','Home Ownership Status:',choices = c("MORTGAGE","NONE","OTHER","OWN","RENT"))
+                                             
+                            ),
 
-                         tabPanel("Prediction", verbatimTextOutput("prediction"))# Regression output
-                     )
-            ),
-            tabPanel("Visualization",
-                     sidebarPanel(
-                         selectInput("Graphtype", "Choose a plot:", 
-                                     choices = c("Loan Category", "Interest rate for each loan grade", "Number of Loan Requests by Home Ownership * Working years",
-                                                 "Frequency by Number of credit lines","Number of Loan Requests by State"
-                                                 ,"plots of interest rate and income","Interest Rate by Number of Payments")),
-                         submitButton("Let's plot it")
-                     ),
-                     mainPanel(
-                         tabPanel("Visualization", plotOutput("Graphs"))
-                         #plotOutput("pie"),plotOutput("scatter"),plotOutput("frequency_loan"),plotOutput("frequency_credit")) # Plot
-                         
-             
-                     )
+                            menuItem("Visualizations",icon = icon("th"),
+                                     menuSubItem('Loan Overview Map',tabName = 'map'),
+                                     menuSubItem("Loan Category", tabName = "pie"),
+                                     menuSubItem("Interest for Loan Grade", tabName = "boxplot"),
+                                     menuSubItem("Frequency by Num_Credit", tabName = "frequency_credit"),
+                                     menuSubItem('Interest by Num_Payment',tabName = 'num_payment'),
+                                     menuSubItem('Loan by Home*Work Year',tabName = 'home_ownership'),
+                                     menuSubItem('Interest and Income',tabName = 'collection')
+                    ))),
+                    dashboardBody(
+                        # Boxes need to be put in a row (or column)
+                        dashboardthemes::shinyDashboardThemes(theme = 'purple_gradient'),
+                       tabItems(
+                           tabItem(tabName = "Prediction",
+                                   h2("Interest Rate Calculator"),
+                                    fluidRow(
+                                       box(title = "Information you entered:",width = 10,
+                                           status = "primary", solidHeader = TRUE, collapsible = TRUE,
+                                           tableOutput("table")
+                                       ),
+                                       box(title = "Prediction:", width = 10, 
+                                           status = "primary",  solidHeader = TRUE, collapsible = TRUE,
+                                           verbatimTextOutput("pred")
+                                   )
+                           )
+                        ),
+                        tabItem(tabName = 'map',
+                                h2('Map'),
+                                fluidRow(
+                                    box(title = 'Amount of Loan Requested in Each State',width = 20, status = 'primary',solidHeader = TRUE,collaspible = TRUE, plotlyOutput('view0'))
+                                )),
+                        
+                            tabItem(tabName = "pie",
+                                    h2("Loan Category"),
+                                    fluidRow(
+                                        box(title = "Loan Category",  width = 8,  background = "red",
+                                            status = "primary", solidHeader = TRUE, collapsible = TRUE,
+                                            plotOutput("view1")
+                                        )
+                                    )
+                            ),
+                            
+                            tabItem(tabName = "boxplot",
+                                    h2("Interest rate for each loan grade"),
+                                    fluidRow(
+                                        box(title = "Interest rate for each loan grade",  width = 11,  background = "red",
+                                            status = "primary", solidHeader = TRUE, collapsible = TRUE,
+                                            plotOutput("view2")
+                                        )
+                                       
+                                    )
+                            ),
+                            
+                            tabItem(tabName = "frequency_credit",
+                                    h2("Frequency by Number of Credit Lines"),
+                                    fluidRow(
+                                        box(title = "Frequency by Number of Credit Lines",  width = 9,  background = "red",
+                                            status = "primary", solidHeader = TRUE, collapsible = TRUE,
+                                            plotlyOutput("view3")
+                                        )
+                                    )
+                            ),
+                        
+                             tabItem(tabName = "num_payment",
+                                h2("Interest Rate by Number of Payments"),
+                                fluidRow(
+                                    box(title = "Frequency by Number of Payment",  width = 9,  background = "red",
+                                        status = "primary", solidHeader = TRUE, collapsible = TRUE,
+                                        plotOutput("view4")
+                                    )
+                                )
+                            ),
+                             tabItem(tabName = "home_ownership",
+                                h2("Number of Loan Requests by Home Ownership * Working years"),
+                                fluidRow(
+                                    box(title = "Number of Loan Requests by Home Ownership * Working years",  width = 9,  background = "red",
+                                        status = "primary", solidHeader = TRUE, collapsible = TRUE,
+                                        plotOutput("view5")
+                                    )
+                                )
+                        ),
+                        
+                            tabItem(tabName = 'collection',
+                                h2('Plots of interest rate and income'),
+                                fluidRow(
+                                    box(title = "Plots of interest rate and income",  width = 9,  background = "red",
+                                        status = "primary", solidHeader = TRUE, collapsible = TRUE,
+                                        plotOutput("view6")
+                                    )
+                                )
+                                )
+                        )
+                        )
                     )
-        )
-    )
 )
-
